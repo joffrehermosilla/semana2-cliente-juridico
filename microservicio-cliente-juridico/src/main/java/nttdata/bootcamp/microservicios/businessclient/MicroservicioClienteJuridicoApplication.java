ce58c7495b4f2f98e20b001be6e94eeff832de50
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 00:11:41 -0500

    primer commit

---
+++ microservicio-cliente-juridico/src/main/java/nttdata/bootcamp/microservicios/businessclient/MicroservicioClienteJuridicoApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.businessclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClienteJuridicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClienteJuridicoApplication.class, args);
+	}
+
+}
---
+++ microservicio-cliente-juridico/src/test/java/nttdata/bootcamp/microservicios/businessclient/MicroservicioClienteJuridicoApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.businessclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClienteJuridicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
